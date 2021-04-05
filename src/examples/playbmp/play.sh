6384642af5fdf85b4dd8a6c7c77f76e3919c5f7e
Ryo Mukai <ryo.m@fancy.ocn.ne.jp>
2021-04-05 13:50:19 +0900

    add -fps option to playbmp

--- src/examples/playbmp/play.sh
+++ src/examples/playbmp/play.sh
@@ -3,4 +3,4 @@
 #MOVIEFILE=Touhou_BadApplePV.mp4
 MOVIEFILE=sample.mp4
 
-ffmpeg -i $MOVIEFILE -s 128x256 -pix_fmt monob -c:v rawvideo -f rawvideo -vf transpose=0 -an - | ./playbmp
+ffmpeg -i $MOVIEFILE -s 128x256 -pix_fmt monob -c:v rawvideo -f rawvideo -vf transpose=0 -an - | ./playbmp -v
--- src/examples/playbmp/playbmp.cpp
+++ src/examples/playbmp/playbmp.cpp
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h> // for usleep
 #include <gu3000graphic.h>
 
 // The raw image data from ffmpeg -c:v rawvideo is MSB first,
 // so settting GU3000PIO::writeByteImage() to MSB first mode
 // is a little bit faster
+
+#define DEFAULT_FPS 29.97
 
 #define MSBFIRST
 //#define LSBFIRST
@@ -24,26 +28,63 @@ inline byte swapbit8(byte x){
 }
 #endif //LSBFIRST
 
-int main(int argc, char *argv[]){
+void usage(){
+  fprintf(stderr, "Usage: playbmp [OPTION]... [FILE]\n");
+  fprintf(stderr, "  -fps FPS: set FPS\n");
+  fprintf(stderr, "  -v, -info: show frame info\n");
+  fprintf(stderr, "  -: FILE=stdin\n");
+  exit(0);
+}
+  int main(int argc, char *argv[]){
   FILE *fp = stdin;
+  const char *filename = NULL;
   VFD vfd;
   char linebuf[1024];
   int n;
   unsigned long time_s;
-  unsigned long time_lastloop = 0;
+  unsigned long time_elapsed = 0;
+
+  bool showinfo = false;
+  double fps = DEFAULT_FPS;
+  useconds_t looptime, usleeptime;
   
 #ifdef MSBFIRST
   vfd.setBitmapOrder(VFD_MSBFIRST);
   vfd.invertFontBitmapOrder();
 #endif
   
-  if(argc > 1){
-    if((fp = fopen(argv[1], "r")) == NULL){
-      fprintf(stderr, "%s: Cannot open file '%s'\n", argv[0], argv[1]);
+  for(int i = 1; i < argc; i++){
+    if(argv[i][0] == '-'){
+      if(strcmp(argv[i], "-fps") == 0){
+	i++;
+	if(i >= argc) usage();
+	fps = atof(argv[i]);
+	if(fps <= 0) usage();
+      } else if(strcmp(argv[i], "-") == 0){
+	fp = stdin;
+      } else if( strcmp(argv[i], "-showinfo") == 0 ||
+		 strcmp(argv[i], "-v") == 0){
+	showinfo = true;
+      } else {
+	usage();
+      }
+    } else {
+      if(filename == NULL){
+	filename = argv[i];
+      } else {
+	usage();
+      }
+    }
+  }
+  if( filename != NULL ){
+    if((fp = fopen(filename, "r")) == NULL){
+      fprintf(stderr, "%s: Cannot open file '%s'\n", argv[0], filename);
       exit(1);
     }
   }
+
   
+  looptime = 1000000.0 / fps;
   while(1){
     time_s = micros();
     if(fread(vfd.buf, sizeof(byte), vfd.bufsize, fp) == 0){
@@ -54,14 +95,22 @@ int main(int argc, char *argv[]){
       vfd.buf[i] = swapbit8(vfd.buf[i]);
     }
 #endif    
-    sprintf(linebuf, "%dusec(%dFPS)\n%d",
-	    (int)(time_lastloop),
-	    time_lastloop ? (int)(1000000.0/time_lastloop): 0,
-	    n++
-	    );
-    vfd.setCursor(0, 0);
-    vfd.puts(linebuf);
+    if(showinfo){
+      sprintf(linebuf, "%dusec(%.2fFPS)\n%d",
+	      (int)(time_elapsed),
+	      time_elapsed ? (double)(1000000.0/time_elapsed): 0,
+	      n++
+	      );
+      vfd.setCursor(0, 0);
+      vfd.puts(linebuf);
+    }
     vfd.show();
-    time_lastloop = micros() - time_s;
+
+    time_elapsed = micros() - time_s;
+    if( looptime > time_elapsed + 100){
+      usleeptime = looptime - time_elapsed - 100;
+      usleep(usleeptime);
+    }
+    time_elapsed = micros() - time_s;
   }
 }
