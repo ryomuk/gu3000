2c307116b990cf237284415085d5f74f486c3c69
Ryo Mukai <ryo.m@fancy.ocn.ne.jp>
2021-04-10 19:34:05 +0900

    fixed 16segments fonts

--- src/font.cpp
+++ src/font.cpp
@@ -5,9 +5,9 @@
 #include "font.h"
 
 #include "fonts/16segments_font.h"
-#include "fonts/hpdl1414_font.h"
 #include "fonts/4x6_font.h"
 #include "fonts/H68TR_font.h"
+#include "fonts/hpdl1414_font.h"
 #include "fonts/MICRlike6x6_font.h"
 #include "fonts/MICRlike7x8_font.h"
 #include "fonts/Noritake6x8_font.h"
@@ -23,10 +23,10 @@ Font *g_FontList[] = {
 		      &font_TK80BS_ASCII,
 		      &font_4x6,
 		      &font_H68TR,
-		      &font_MICRlike7x8,
-		      &font_MICRlike6x6,
 		      &font_16segments,
 		      &font_hpdl1414,
+		      &font_MICRlike7x8,
+		      &font_MICRlike6x6,
 		      &font_TK80BS,
 		      &font_TK80BS_KANA,
 		      &font_TK80BS_GREEK,
